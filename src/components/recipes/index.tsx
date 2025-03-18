1254ea83a8ba1b1a215d58d40caecdb826ebbcb7
AnnMath <pap90@yahoo.com>
2025-03-18 11:50:03 +0100

    * Redesign cards * add size prop to StarRating for easy reuseability * use Math.round in StarRating to better represent rating

--- src/components/recipes/index.tsx
+++ src/components/recipes/index.tsx
@@ -5,7 +5,7 @@ import RecipeListCard from './recipe-list-card'
 const Recipes = async ({ recipes }: { recipes: Promise<Recipe[]> }) => {
   const allRecipes = await recipes
   return (
-    <ul className="flex flex-col flex-wrap gap-8 mt-4 sm:grid sm:grid-cols-2">
+    <ul className="flex flex-wrap justify-evenly gap-8 mt-4 mx-auto">
       {allRecipes.map((recipe) => (
         <li key={recipe.id}>
           <Link href={`/recipe/${recipe.id}`}>
