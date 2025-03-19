16e6bda4b5037fdb3975057e55002c3a7e3b1f12
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-19 16:22:58 +0100

    #1_create functionality to capture the input/search value and to call the search function that fetches data

--- src/components/search-input/search-input.tsx
+++ src/components/search-input/search-input.tsx
@@ -1,22 +1,55 @@
+'use client'
+
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Search } from 'lucide-react'
+import { fetchSearchResults } from '@/lib/recipes/actions'
 
 export function SearchInput() {
+  const [query, setQuery] = useState('')
+  const [results, setResults] = useState([])
+  const router = useRouter()
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(event.target.value)
+  }
+
+  const handleSearch = async () => {
+    const data = await fetchSearchResults(query)
+    router.push(`/recipes/search/${query}`)
+    setResults(data)
+  }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      handleSearch()
+    }
+  }
+
   return (
-    <section className="flex flex-col gap-3 w-full max-w-sm items-center mx-auto">
+    <section className="flex flex-col gap-3 w-full max-w-sm items-center mx-auto mt-10 mb-20">
       <label htmlFor="search" className="text-2xl font-bold">
         Search for recipes
       </label>
       <div className="flex w-full">
         <Input
           id="search"
-          type="search"
+          type="text"
+          value={query}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
           placeholder="Search recipe..."
           className="rounded-r-none rounded-l-full"
         />
 
-        <Button type="submit" className="rounded-l-none rounded-r-full">
+        <Button
+          onClick={handleSearch}
+          type="submit"
+          className="rounded-l-none rounded-r-full"
+        >
           <Search />
         </Button>
       </div>
