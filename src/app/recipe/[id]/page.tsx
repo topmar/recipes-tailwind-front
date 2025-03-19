7331b65f2b693812a58a511a78eb4ac70e15a169
Sandra Höst Kannerberg <sandra.hkannerberg@gmail.com>
2025-03-19 09:49:30 +0100

    #32_responsive grid layout

--- src/app/recipe/[id]/page.tsx
+++ src/app/recipe/[id]/page.tsx
@@ -18,10 +18,22 @@ export default async function RecipeDetailPage({
   return (
     <main>
       {/* Page wrapper */}
-      <div className="grid grid-cols-2 mx-10 xl:mx-56">
-        <h1 className="text-5xl font-bold col-span-2 my-10">{recipe.name}</h1>
+      <div className="grid md:grid-cols-2 mx-3 sm:mx-10 xl:mx-56">
+        <h1 className="text-4xl md:text-5xl font-bold my-10 md:col-span-2">
+          {recipe.name}
+        </h1>
 
-        <section className="flex flex-col gap-5 bg-orange-50 p-10">
+        <figure className="w-full h-full md:col-start-2">
+          <Image
+            src={recipe.image}
+            width={800}
+            height={800}
+            alt={recipe.name}
+            className="w-full h-full object-cover"
+          />
+        </figure>
+
+        <section className="flex flex-col gap-10 p-5 md:p-10 bg-orange-50 md:row-start-2 md:col-start-1">
           <RecipeInfo
             prepTimeMinutes={recipe.prepTimeMinutes}
             cookTimeMinutes={recipe.cookTimeMinutes}
@@ -34,19 +46,13 @@ export default async function RecipeDetailPage({
             mealType={recipe.mealType}
           />
 
-          <StarRating rating={recipe.rating} size={25} />
+          <div className="flex gap-3">
+            <StarRating rating={recipe.rating} size={25} />
+            <span>{recipe.rating}</span>
+          </div>
 
           <RecipeDescription name={recipe.name} cuisine={recipe.cuisine} />
         </section>
-        <figure className="w-full h-full">
-          <Image
-            src={recipe.image}
-            width={800}
-            height={800}
-            alt={recipe.name}
-            className="w-full h-full object-cover"
-          />
-        </figure>
 
         <IngredientsList ingredients={recipe.ingredients} />
         <Instructions instructions={recipe.instructions} />
