7dbcf39e36ec6fc312323d41872a49d5d79a9276
AnnMath <pap90@yahoo.com>
2025-03-20 14:20:49 +0100

    janky hack to make the Select component a bit prettier on reset

--- src/components/ui/sort-dropdown.tsx
+++ src/components/ui/sort-dropdown.tsx
@@ -9,10 +9,12 @@ import {
   SelectValue,
   SelectLabel
 } from '@/components/ui/select'
-import { ArrowDownAZ, ArrowUpAZ } from 'lucide-react'
+import { ArrowDownAZ, ArrowUpAZ, RotateCcw } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { useState } from 'react'
 
 const SortDropdown = () => {
+  const [selectKey, setSelectKey] = useState(Date.now())
   const router = useRouter()
   const searchParams = useSearchParams()
 
@@ -21,6 +23,15 @@ const SortDropdown = () => {
     params.set('sortBy', 'name')
     params.set('order', order)
 
+    // reset to default order
+    if (order === 'none') {
+      params.delete('sortBy')
+      params.delete('order')
+      // the shadcn Select has no native reset, so this is a really janky way of forcing a rerender
+      // so that when reset is clicked, the value is changed to "" and the placeholder shows again, instead of the last selected item
+      setSelectKey(Date.now())
+    }
+
     // reset to page 1 if we change the order
     params.set('page', '1')
 
@@ -28,7 +39,7 @@ const SortDropdown = () => {
   }
   return (
     <>
-      <Select onValueChange={(value) => handleChange(value)} value="">
+      <Select key={selectKey} onValueChange={(value) => handleChange(value)}>
         <SelectTrigger className="w-28">
           <SelectValue placeholder="Order by" />
         </SelectTrigger>
@@ -40,6 +51,12 @@ const SortDropdown = () => {
             </SelectItem>
             <SelectItem value="desc">
               <ArrowUpAZ />
+            </SelectItem>
+          </SelectGroup>
+          <SelectGroup>
+            <SelectLabel>Reset</SelectLabel>
+            <SelectItem value={'none'}>
+              <RotateCcw />
             </SelectItem>
           </SelectGroup>
         </SelectContent>
